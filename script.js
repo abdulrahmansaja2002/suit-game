d0e91ee665f175181d43f9d664a39565e5249709
abdulrahmansaja2002 <89397339+abdulrahmansaja2002@users.noreply.github.com>
2021-10-19 17:12:56 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+const imgs = document.querySelectorAll(".imgs .img img");
+function compChosen() {
+    const comp = Math.random();
+    if ( comp < 0.34) return "batu";
+    if ( comp > 0.67) return "kertas";
+    return "gunting";   
+}
+function getResult(comp, player) {
+    if (comp == player) return "DRAW!!!";
+    if (comp == "batu") return (player == "kertas") ? "YOU WIN!" : "YOU LOSE";
+    if (comp == "kertas") return (player == "gunting")? "YOU WIN!" : "YOU LOSE";
+    if (comp == "gunting") return (player == "batu") ? "YOU WIN!" : "YOU LOSE";
+}
+function makeRandom() {
+    const imgComp = document.querySelector("img[alt=imgComp]");
+    const imgs = [
+        "img/batu.png",
+        "img/gunting.png",
+        "img/kertas.png",
+    ]
+    let i = 0;
+    const start = new Date().getTime();
+    setInterval(function(){
+        if (new Date().getTime() - start > 1000){
+            return;
+        }
+        imgComp.setAttribute("src", imgs[i%3]);
+        i++;
+    }, 50); // 50 = 0.05 detik
+}
+imgs.forEach(function(el){
+    // console.log(el.getAttribute("alt"));
+
+    el.addEventListener('click', function(e){
+        const imgComp = document.querySelector("img[alt=imgComp]");
+        // console.log(imgComp);
+        const info = document.getElementsByClassName("result")[0];
+
+        const player = e.target.getAttribute("alt");
+        const comp = compChosen();
+        const result = getResult(comp, player);
+        makeRandom();
+        setTimeout(function(){
+            imgComp.setAttribute("src", "img/"+comp+".png");
+            info.innerHTML = result;
+        },1000);
+
+
+    });
+});
+// console.log();
